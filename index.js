c82fe3bf98291b84e92098c5f03bb26314222648
harshitpandey0426 <harshit.pandey@students.iiit.ac.in>
2020-06-12 17:23:16 +0530

    just trying

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express()
 
 app.get('/',(req,res) => {
-    res.send({hi:'there'});
+    res.send({Tikku_Baby:'I miss you <3'});
 });
 const PORT=process.env.PORT || 5000;
 
