b1d13f28aaaecb46538c9e423db5bc44c5be6cf3
harshitpandey0426 <harshit.pandey@students.iiit.ac.in>
2020-06-14 03:04:27 +0530

    mongo duplicate

--- services/passport.js
+++ services/passport.js
@@ -10,7 +10,18 @@ passport.use(new GoogleStrategy({
     callbackURL:'/auth/google/callback'
 },
 (accessToken,refreshToken,profile,done) =>{   //This fetch information from google about my account, which will be visible on console
-    new User({googleID : profile.id}).save(); //.save() will save it to our database record, profile.id is the id coming from users profile ,new User wil create new user which is a modelclass
+    
+    User.findOne({googleId : profile.id})
+    .then((existinguser)=>{
+        if(existinguser)
+        {
+            // If we already have arecord with this id
+        }
+        else{
+            // if dont have record with this id
+            new User({googleID : profile.id}).save(); //.save() will save it to our database record, profile.id is the id coming from users profile ,new User wil create new user which is a modelclass
+        }
+    })
     // console.log('access token',accessToken);  //access token ko he google dekhega aur smjhega ki isko ko maine request grant ki thi photo ya email me changes ki to ye it google can allow this request to access.
     // console.log('refresh token',refreshToken);// we are not using access and refresh token, but access token gets refreshed in small amount of time, so it is related to it.
     // console.log('profile:',profile); // this is what we will be using in our project
